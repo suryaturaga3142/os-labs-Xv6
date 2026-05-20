5d054d305ed9597b327aeaf3e9b1a5860ba8948b
Surya Turaga <suryapratheek.turaga@gmail.com>
2026-05-20 12:22:57 +0530

    First commit

---
+++ user/sleep.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main (int argc, char *argv[]) {
+    if (argc <= 1) {
+        fprintf(2, "Usage: sleep <seconds>\n");
+        exit (1);
+    }
+    
+    int t = atoi(argv[1]);
+
+    pause(10 * t);
+    
+    exit (0);
+}
